021beb877045efb5a20e8d4b9caab476ce4f3234
yallyyally <jhshin0028@gmail.com>
2020-07-01 14:01:50 +0900

    final
    DB 연결 필요

--- app/main/index.py
+++ app/main/index.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, request, render_template, flash, redirect, url_for
 from flask import current_app as app
 from app.module import dbModule
+from datetime import datetime,timedelta,date
 
+today = date.today()
+sevenWeek = []
+for i in range(7):
+    day= today-timedelta(7-i)
+    sevenWeek.append(day)       
 #index 파일 들어갔을때 이름 설정 , url 어떻게 붙일 지,,,
 main= Blueprint('main', __name__, url_prefix='/')
 
@@ -14,12 +20,31 @@ def index():
 @main.route("/RealTimePrice",methods = ['GET'])
 def RealTimePrice():
     db_class = dbModule.Database()
-    #평균값. 
-    avg_tmon = db_class.selectDataAsShop('Nintendo_TB','TMON')
-    avg_naver = db_class.selectDataAsShop('Nintendo_TB','NAVER Shopping')
-    avg_auction = db_class.selectDataAsShop('Nintendo_TB','Auction')
+    #최신값. 
+    avg_tmon = db_class.selectLatestDataAsShop('Nintendo_TB','TMON')
+    avg_naver = db_class.selectLatestDataAsShop('Nintendo_TB','NAVER Shopping')
+    avg_auction = db_class.selectLatestDataAsShop('Nintendo_TB','Auction')
     return render_template('/main/RealTimePrice.html',resultTmon = avg_tmon,resultNaver = avg_naver,resultAuction = avg_auction)
 
 @main.route("/PriceChange",methods = ['GET'])
 def PriceChange():
-   return render_template('main/PriceChange.html')
+    db_class = dbModule.Database()
+    #평균값. 
+    avg_tmon=[]
+    avg_naver =[]
+    avg_auction=[]
+    for i in range(7):
+        print(sevenWeek[i])
+        avg_tmon.append(db_class.selectAvgDataAsShopPerDay('Nintendo_TB','TMON',sevenWeek[i]))
+        avg_naver.append(db_class.selectAvgDataAsShopPerDay('Nintendo_TB','Naver Shopping',sevenWeek[i]))
+        avg_auction.append(db_class.selectAvgDataAsShopPerDay('Nintendo_TB','Auction',sevenWeek[i]))
+    return render_template('main/PriceChange.html',resultTmon = avg_tmon,resultNaver = avg_naver,resultAuction = avg_auction)
+   
+@main.route("/Market",methods = ['GET'])
+def Market():
+   return render_template('main/Market_frame.html')
+
+@main.route("/Login",methods = ['GET'])
+def Login():
+   return render_template('main/Login.html')
+
--- app/module/dbModule.py
+++ app/module/dbModule.py
@@ -23,6 +23,7 @@ class Database():
         return row
 
     #tuple 다
+    ## 실시간: 오늘꺼 ->(row 막줄) 
     def executeAll(self,query,args={}):
         self.cursor.execute(query,args)
         row = self.cursor.fetchall()
@@ -31,86 +32,73 @@ class Database():
     def commit():
         self.db.commit()
 
-    #특정 쇼핑몰(ShopName)에서 해당 상품(TableName)의 평균 가격을 반환한다.
-    def selectDataAsShop(self,TableName,ShopName):
+    #특정 쇼핑몰(ShopName)에서 해당 상품(TableName),해당 날짜(Date) , 해당 시간 (Time)에서의 평균 가격을 반환한다.
+    def selectAvgDataAsShopPerHour(self,TableName,ShopName,Date,TimeFrom,TimeTo):
         ShopName = "'"+ShopName+"'"
         AvgPrice = 0
         count=0
         with self.db.cursor() as cursor:
-            # table 조회-해당 쇼핑몰의 data만 조회
-            sql = """SELECT price FROM %s WHERE shop=%s;"""
-            cursor.execute(sql%(TableName,ShopName,))
+            sql = """SELECT price FROM %s WHERE shop=%s and pdate BETWEEN '%s %s' AND '%s %s';"""
+            cursor.execute(sql%(TableName,ShopName,Date,TimeFrom,Date,TimeTo))
             result = cursor.fetchall()
 
             for i in result:
                 #1.문자열의 반점(,) 제거
                 ##1-1.반점으로 나눈다.
                 PriceFromStr = i[0].split(',')
-             ##1-2.다시 join 한다.
+                 ##1-2.다시 join 한다.
                 PriceFromStr = ''.join(PriceFromStr)
              #2.문자열->정수로 변환
                 PriceFromStr = int(PriceFromStr)
              #3.합산하여 평균 구하기
                 AvgPrice += PriceFromStr
                 count += 1
-
         AvgPrice = int(AvgPrice/count)
         return AvgPrice
 
+ #특정 쇼핑몰(ShopName)에서 해당 상품(TableName),해당 날짜(Date) 의 평균 가격을 반환한다.
+    def selectAvgDataAsShopPerDay(self,TableName,ShopName,Date):
+        ShopName = "'"+ShopName+"'"
+        AvgPrice = 0
+        count=0
+        with self.db.cursor() as cursor:
+            sql = """SELECT price FROM %s WHERE shop=%s and pdate BETWEEN '%s 00:00:00' AND '%s 23:59:59';"""
+            cursor.execute(sql%(TableName,ShopName,Date,Date))
+            result = cursor.fetchall()
+
+            for i in result:
+                #1.문자열의 반점(,) 제거
+                ##1-1.반점으로 나눈다.
+                PriceFromStr = i[0].split(',')
+                 ##1-2.다시 join 한다.
+                PriceFromStr = ''.join(PriceFromStr)
+             #2.문자열->정수로 변환
+                PriceFromStr = int(PriceFromStr)
+             #3.합산하여 평균 구하기
+                AvgPrice += PriceFromStr
+                count += 1
+        if(count==0):
+            return 600152
+        else:
+           AvgPrice = int(AvgPrice/count)
+        return AvgPrice
+
+    #특정 쇼핑몰(ShopName)에서 해당 상품(TableName)의 최신 가격을 반환한다.
+    def selectLatestDataAsShop(self,TableName,ShopName):
+        ShopName = "'"+ShopName+"'"
+        AvgPrice = 0
+        count=0
+        with self.db.cursor() as cursor:
+            sql = """SELECT price FROM %s WHERE shop=%s;"""
+            cursor.execute(sql%(TableName,ShopName,))
+            result = cursor.fetchone()
+            
+            #1.문자열의 반점(,) 제거
+            ##1-1.반점으로 나눈다.
+            PriceFromStr = result[0].split(',')
+            PriceFromStr = ''.join(PriceFromStr)
+            PriceFromStr = int(PriceFromStr)
+
+        return PriceFromStr
 
 
-    #이건 이미 마이스큐엘에 있으니까 실행할 필요 없을듯?
-    #def makeTable(TableName):
-        #with self.db.cursor() as cursor:
-            # table 생성
-            #sql = """
-                #CREATE TABLE %s(
-                    #shop VARCHAR(20),
-                    #pname VARCHAR(80),
-                    #price VARCHAR(20),
-                    #pdate DATETIME
-                #);"""
-            #cursor.execute(sql%(TableName,))
-            #self.db.commit()
-
-   #  def insertData(filepath,TableName):
-    #    data = (filepath,TableName)
-    # with db.cursor() as cursor:
-        # row 삽입
-        ##중복되면 안넣는 코드 추가,,,,,,
-     #       sql = """LOAD DATA INFILE '%s' INTO TABLE %s;"""% (filepath,TableName)
-      #   cursor.execute(sql)
-       #  db.commit()
-
-    #def selectData(TableName):
-    #with db.cursor() as cursor:
-        # table 조회
-     #   sql = """SELECT * FROM %s;"""
-      #  cursor.execute(sql%(TableName,))
-        #실행 결과 가져오기
-        ##db.commit() 실행시키지 않는 이유 - 내부 데이터에 영향 X, 조회만 했기 때문
-       # result = cursor.fetchall()
-       # for i in result:
-        #    print(i)
-#try:
-    # table 생성 (한번만 실행)
-#    makeTable('Nintendo_TB')
-
-    #txt file->table삽입 (한번만 실행)
-    ##이때 파일 경로 -> my.ini 파일에서 secure-file-priv.
-    ###상품이 더 추가될 경우 대비-> 인자로 테이블 명(제품 종류에 따라 다른 테이블)을 준다.
-#    insertData('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sample_data.txt','Nintendo_TB')
-
-    #data 조회 -> 실시간 조회의 경우
-    ##상품이 더 추가될 경우 대비-> 인자로 테이블 명(제품 종류에 따라 다른 테이블)을 준다.
-#    selectData('Nintendo_TB')
-
-    ###각 쇼핑몰마다 조회할 수 있도록
- #   Nintendo_Naver = selectDataAsShop('Nintendo_TB','NAVER Shopping')
-  #  Nintendo_Tmon = selectDataAsShop('Nintendo_TB','TMON')
-  #  Nintendo_Auction = selectDataAsShop('Nintendo_TB','Auction')
-
-#except Exception as e:
- #   print(e)
-#finally:
- #   db.close()
